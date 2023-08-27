e7bff1176b170fceac575a65e3240305d4748b7c
周诚 <zhoucheng133@outlook.com>
2023-08-27 19:55:28 +0800

    添加播放器组件

---
+++ lib/functions/player.dart
@@ -0,0 +1,24 @@
+import 'package:audio_service/audio_service.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:get/get.dart';
+import 'package:netplayer_mobile/para/para.dart';
+
+class MyAudioHandler extends BaseAudioHandler with QueueHandler, SeekHandler {
+  final Controller c = Get.put(Controller());
+  final player = AudioPlayer();
+  
+  @override
+  Future<void> play() async {
+    
+  }
+
+  @override
+  Future<void> pause() async {
+    
+  }
+
+  @override
+  Future<void> skipToNext()async {
+    
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import audio_service
+import audio_session
+import audioplayers_darwin
+import path_provider_foundation
 import shared_preferences_foundation
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
