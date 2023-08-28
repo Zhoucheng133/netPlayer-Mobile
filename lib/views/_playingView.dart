c03689f205c1c3753b3e9350122df68cff0d907e
周诚 <zhoucheng133@outlook.com>
2023-08-28 22:18:35 +0800

    添加播放页面

---
+++ lib/views/_playingView.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: camel_case_types, file_names
+
+import 'package:flutter/material.dart';
+
+class playingView extends StatefulWidget {
+  const playingView({super.key});
+
+  @override
+  State<playingView> createState() => _playingViewState();
+}
+
+class _playingViewState extends State<playingView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
