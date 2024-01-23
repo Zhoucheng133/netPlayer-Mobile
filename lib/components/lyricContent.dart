599c178117c730b38e360bcffbfa87eeaf89d012
zhoucheng <zhoucheng133@outlook.com>
2024-01-23 17:27:14 +0800

    去除不需要的导入

--- lib/components/lyricContent.dart
+++ lib/components/lyricContent.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: file_names, camel_case_types, prefer_const_constructors
 
-// import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:netplayer_mobile/para/para.dart';
