62585dad5db050969ca35c49d12b7811a548e989
zhoucheng133 <zhoucheng133@outlook.com>
2024-03-13 14:20:43 +0800

    添加修改状态入口

--- lib/components/bottomArea.dart
+++ lib/components/bottomArea.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:netplayer_mobile/para/para.dart';
 
@@ -17,6 +19,10 @@ class BottomArea extends StatefulWidget {
 class _BottomAreaState extends State<BottomArea> {
 
   final Controller c = Get.put(Controller());
+
+  void changeContent(String type){
+
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -24,21 +30,34 @@ class _BottomAreaState extends State<BottomArea> {
       children: [
         SizedBox(
           height: 60,
-          width: 200,
+          width: 220,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Icon(Icons.lyrics),
-              Obx(() => 
-                c.fullRandom.value ? Icon(
-                  Icons.shuffle_rounded,
-                  color: Colors.grey,
-                ) : 
-                c.playMode.value=="随机播放" ? Icon(Icons.shuffle_rounded) : 
-                c.playMode.value=="顺序播放" ? Icon(Icons.repeat_rounded) :
-                Icon(Icons.repeat_one_rounded),
+              GestureDetector(
+                onTap: () => changeContent("lyric"),
+                child: Icon(Icons.lyrics),
               ),
-              Icon(Icons.more_horiz_rounded)
+              GestureDetector(
+                onTap: () => changeContent("playMode"),
+                child: Obx(() => 
+                  c.fullRandom.value ? Icon(
+                    Icons.shuffle_rounded,
+                    color: Colors.grey,
+                  ) : 
+                  c.playMode.value=="随机播放" ? Icon(Icons.shuffle_rounded) : 
+                  c.playMode.value=="顺序播放" ? Icon(Icons.repeat_rounded) :
+                  Icon(Icons.repeat_one_rounded),
+                ),
+              ),
+              GestureDetector(
+                onTap: () => changeContent("add"),
+                child: Icon(Icons.add_rounded)
+              ),
+              GestureDetector(
+                onTap: () => changeContent("more"),
+                child: Icon(Icons.more_horiz_rounded)
+              ),
             ],
           ),
         )
