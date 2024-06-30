12866b4b7d5704289ab930cef88be1451c7675b9
周诚 <zhoucheng133@outlook.com>
2024-06-30 19:19:45 +0800

    重置项目

--- lib/main.dart
+++ lib/main.dart
@@ -1,154 +1,125 @@
-// ignore_for_file: prefer_const_constructors, unrelated_type_equality_checks, prefer_typing_uninitialized_variables, unused_element, prefer_const_literals_to_create_immutables
-
-import 'dart:convert';
-
-import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:get/get.dart';
-import 'package:netplayer_mobile/functions/player.dart';
-import 'package:netplayer_mobile/para/para.dart';
-import 'package:netplayer_mobile/views/_mainView.dart';
-import 'package:netplayer_mobile/views/_loginView.dart';
-// import 'package:netplayer_mobile/views/_playingView.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-var _audioHandler;
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  _audioHandler = await AudioService.init(
-    builder: () => MyAudioHandler(),
-    config: AudioServiceConfig(
-      androidNotificationChannelId: 'com.mycompany.myapp.channel.audio',
-      androidNotificationChannelName: 'Music playback',
-    ),
-  );
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    statusBarColor: Colors.transparent,
-    systemNavigationBarColor: Colors.transparent,
-  ));
-  runApp(MainApp());
+void main() {
+  runApp(const MyApp());
 }
 
-class MainApp extends StatefulWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  @override
-  State<MainApp> createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> with WidgetsBindingObserver {
-  final Controller c = Get.put(Controller());
-
-  var isLoaded=false;
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.resumed) {
-      // 应用进入前台
-      c.updateFronted(true);
-    } else {
-      // 应用进入后台
-      c.updateFronted(false);
-    }
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  Future<void> checkLogin() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final String? userData = prefs.getString('userInfo');
-    final String? playInfo = prefs.getString('playInfo');
-    final bool? autoLogin = prefs.getBool('autologin');
-    final bool? savePlay = prefs.getBool('savePlay');
-
-    if(userData!=null && (autoLogin==true || autoLogin==null)){
-      c.updateLogin(true);
-      Map<String,dynamic> decodeUserData = json.decode(userData);
-      c.updateUserInfo(decodeUserData);
-    }
-
-    if(autoLogin==false){
-      c.autoLogin.value=false;
-    }
-
-    if(savePlay!=false && playInfo!=null){
-      Map<String,dynamic> decodePlayInfo = json.decode(playInfo);
-      if(decodePlayInfo.isNotEmpty){
-        c.updatePlayInfo(decodePlayInfo);
-      }
-    }
-
-    // print(c.playInfo);
-
-    if(savePlay==false){
-      c.savePlay.value=false;
-    }
-    
-    setState(() {
-      isLoaded=true;
-    });
-  }
-
-  Future<void> setPlayMode() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final bool? isFullRandom = prefs.getBool('isFullRandom');
-    if(isFullRandom==true){
-      c.updateFullRandom(true);
-      c.updatePlayMode("随机播放");
-    }
-    // 获取上一次播放模式
-    final String? playMode=prefs.getString("playMode");
-    if(playMode!=null){
-      c.updatePlayMode(playMode);
-    }
-  }
-  
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-
-    // 检查登录
-    checkLogin();
-    setPlayMode();
-  }
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate
-      ],
-      supportedLocales: [
-        Locale('en', 'US'), // 美国英语
-        Locale('zh', 'CN'), // 中文简体
-      ],
+      title: 'Flutter Demo',
       theme: ThemeData(
-        fontFamily: "Noto",
-        highlightColor: Colors.transparent,
-        splashColor: Colors.transparent,
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        resizeToAvoidBottomInset: false,
-        body:  isLoaded ? Obx(
-          () => AnimatedSwitcher(
-            duration: Duration(milliseconds: 200),
-            child: c.isLogin==true ? mainView(audioHandler: _audioHandler,) : loginView(),
-          )
-        ) : Container(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
