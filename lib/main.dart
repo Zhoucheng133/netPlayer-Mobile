ff39d45654a0f20d1675bc7a1da3dde30c06cb04
周诚 <zhoucheng133@outlook.com>
2024-07-22 18:26:34 +0800

    添加语言支持

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main(){
   runApp(const MainApp());
@@ -10,6 +11,16 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
+      supportedLocales: [
+        Locale('en', 'US'),
+        Locale('zh', 'CN'),
+      ],
       home: Scaffold(
         body: Center(
           child: Text("Hello world!"),
