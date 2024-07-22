5a2932fb7e6a287607ccc03916f9f21d73e5ec4d
周诚 <zhoucheng133@outlook.com>
2024-07-22 19:49:53 +0800

    添加静态颜色类

---
+++ lib/variables/static_color.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class StaticColor{
+  // 一些颜色样本，从浅色=>深色
+  var color1=const Color.fromRGBO(248, 249, 255, 1);
+  var color2=const Color.fromARGB(255, 238, 241, 255);
+  var color3=const Color.fromARGB(255, 233, 236, 255);
+  var color4=const Color.fromARGB(255, 179, 189, 252);
+  var color5=const Color.fromARGB(255, 152, 166, 254);
+  var color6=const Color.fromARGB(255, 120, 135, 232);
+}
