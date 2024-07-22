b42c10c385a0ccc00d7e122dddeb49227cb1e096
周诚 <zhoucheng133@outlook.com>
2024-07-22 19:50:17 +0800

    添加一些新页面

---
+++ lib/pages/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
