babcf5f5923dd1c9c04cf0743694438d0a91906c
周诚 <zhoucheng133@outlook.com>
2024-07-23 19:37:05 +0800

    添加设置页面

---
+++ lib/pages/settings.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class Settings extends StatefulWidget {
+  const Settings({super.key});
+
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
