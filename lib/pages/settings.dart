3892e9567bb0a80a0ff7ee7bac060b210ab356b8
周诚 <zhoucheng133@outlook.com>
2024-08-22 18:02:57 +0800

    添加设置选项卡

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,7 +1,9 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:netplayer_mobile/pages/components/title_aria.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Settings extends StatefulWidget {
   const Settings({super.key});
@@ -11,6 +13,10 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+
+  bool enableAutoLogin=true;
+  bool savePlay=true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +29,67 @@ class _SettingsState extends State<Settings> {
         children: [
           TitleAria(title: '设置', subtitle: ' '),
           Expanded(
-            child: Container(),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: ListView(
+                children: [
+                  SizedBox(
+                    height: 60,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            '自动登录',
+                            style: GoogleFonts.notoSansSc(
+                              fontSize: 18
+                            ),
+                          ),
+                        ),
+                        Switch(
+                          activeTrackColor: Colors.blue,
+                          value: enableAutoLogin, 
+                          onChanged: (val) async {
+                            setState(() {
+                              enableAutoLogin=val;
+                            });
+                            final SharedPreferences prefs = await SharedPreferences.getInstance();
+                            prefs.setBool('autoLogin', val);
+                          }
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 60,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            '保存上次播放位置',
+                            style: GoogleFonts.notoSansSc(
+                              fontSize: 18
+                            ),
+                          ),
+                        ),
+                        Switch(
+                          activeTrackColor: Colors.blue,
+                          value: savePlay, 
+                          onChanged: (val) async {
+                            setState(() {
+                              savePlay=val;
+                            });
+                            final SharedPreferences prefs = await SharedPreferences.getInstance();
+                            prefs.setBool('savePlaty', val);
+                          }
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
           ),
         ],
       ),
