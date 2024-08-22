50ac3464bcc8f927de218f46afa84b400ec5e689
周诚 <zhoucheng133@outlook.com>
2024-08-22 18:25:02 +0800

    添加歌曲item的一些入口

--- lib/pages/components/song_item.dart
+++ lib/pages/components/song_item.dart
@@ -14,50 +14,64 @@ class SongItem extends StatefulWidget {
 class _SongItemState extends State<SongItem> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 60,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(
-            width: 30,
-            child: Center(child: Text((widget.index+1).toString())),
-          ),
-          const SizedBox(width: 10,),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  widget.item['title'],
-                  overflow: TextOverflow.ellipsis,
-                  style: GoogleFonts.notoSansSc(
-                    fontSize: 16,
-                  ),
-                ),
-                Text(
-                  widget.item['artist'],
-                  overflow: TextOverflow.ellipsis,
-                  style: GoogleFonts.notoSansSc(
-                    fontSize: 12,
-                    color: Colors.grey[400]
-                  ),
-                )
-              ],
+    return GestureDetector(
+      onTap: (){
+        print("播放~~");
+        // TODO 播放歌曲
+      },
+      child: Container(
+        color: Colors.transparent,
+        height: 60,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 30,
+              child: Center(child: Text((widget.index+1).toString())),
             ),
-          ),
-          const SizedBox(width: 10,),
-          const SizedBox(
-            width: 50,
-            child: Center(
-              child: Icon(
-                Icons.more_vert_rounded,
-                size: 20,
+            const SizedBox(width: 10,),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.item['title'],
+                    overflow: TextOverflow.ellipsis,
+                    style: GoogleFonts.notoSansSc(
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    widget.item['artist'],
+                    overflow: TextOverflow.ellipsis,
+                    style: GoogleFonts.notoSansSc(
+                      fontSize: 12,
+                      color: Colors.grey[400]
+                    ),
+                  )
+                ],
               ),
             ),
-          )
-        ],
+            const SizedBox(width: 10,),
+            GestureDetector(
+              onTap: (){
+                print("详细~~");
+                // TODO 歌曲详细
+              },
+              child: Container(
+                color: Colors.transparent,
+                width: 50,
+                child: const Center(
+                  child: Icon(
+                    Icons.more_vert_rounded,
+                    size: 20,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
