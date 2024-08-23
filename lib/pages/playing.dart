1296832a0c58ca116c3c7653461b719ea4c1c175
周诚 <zhoucheng133@outlook.com>
2024-08-23 19:36:27 +0800

    完成播放页时间滚动条

--- lib/pages/playing.dart
+++ lib/pages/playing.dart
@@ -1,5 +1,7 @@
+import 'package:easy_debounce/easy_debounce.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:netplayer_mobile/pages/components/title_aria.dart';
 import 'package:netplayer_mobile/variables/player_var.dart';
 import 'package:netplayer_mobile/variables/user_var.dart';
@@ -15,6 +17,30 @@ class _PlayingState extends State<Playing> {
 
   PlayerVar p=Get.put(PlayerVar());
   final UserVar u = Get.put(UserVar());
+
+  void seekSong(val){
+    if(p.nowPlay['id'].isEmpty){
+      return;
+    }
+    p.handler.pause();
+    int progress=(p.nowPlay['duration']*1000*val).toInt();
+    p.playProgress.value=progress;
+    EasyDebounce.debounce(
+      'slider',
+      const Duration(milliseconds: 50),
+      () {
+        p.handler.seek(Duration(milliseconds: p.playProgress.value));
+        p.handler.play();
+      }
+    );
+  }
+
+  String convertDuration(int time){
+    int min = time ~/ 60;
+    int sec = time % 60;
+    String formattedSec = sec.toString().padLeft(2, '0');
+    return "$min:$formattedSec";
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,10 +66,10 @@ class _PlayingState extends State<Playing> {
               Expanded(
                 child: Container(
                   color: Colors.transparent,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Obx(()=>
+                  child: Obx(()=>
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
                         Container(
                           height: MediaQuery.of(context).size.width-150,
                           width: MediaQuery.of(context).size.width-150,
@@ -53,25 +79,133 @@ class _PlayingState extends State<Playing> {
                               const AssetImage("assets/blank.jpg")
                             )
                           ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 30, right: 30, top: 40),
+                          child: Stack(
+                            children: [
+                              SliderTheme(
+                                data: SliderThemeData(
+                                  overlayColor: Colors.transparent,
+                                  overlayShape: const RoundSliderOverlayShape(overlayRadius: 0.0), // 取消波纹效果
+                                  activeTrackColor: Colors.black, // 设置已激活轨道的颜色
+                                  inactiveTrackColor: Colors.grey[200], 
+                                  trackHeight: 2,
+                                  thumbShape: const RoundSliderThumbShape(
+                                    enabledThumbRadius: 8,
+                                    pressedElevation: 0,
+                                    elevation: 1,
+                                    
+                                  ),
+                                  thumbColor: Colors.black
+                                ),
+                                child: Slider(
+                                  value: p.nowPlay['duration']==0 ? 0.0 : p.playProgress.value/1000/p.nowPlay["duration"]>1 ? 1.0 : p.playProgress.value/1000/p.nowPlay["duration"]<0 ? 0 : p.playProgress.value/1000/p.nowPlay["duration"], 
+                                  onChanged: (value){
+                                    seekSong(value);
+                                  }
+                                ),
+                              ),
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width - 60,
+                                child: Padding(
+                                  padding: const EdgeInsets.only(top: 20),
+                                  child: Row(
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      Obx(()=>
+                                        Text(
+                                          p.nowPlay['duration']==0 ? "" : convertDuration(p.playProgress.value~/1000),
+                                          style: GoogleFonts.notoSansSc(
+                                            fontSize: 12,
+                                            color: Colors.black
+                                          ),
+                                        )
+                                      ),
+                                      Expanded(child: Container()),
+                                      Obx(()=>
+                                        Text(
+                                          p.nowPlay['duration']==0 ? "" : convertDuration(p.nowPlay['duration']),
+                                          style: GoogleFonts.notoSansSc(
+                                            fontSize: 12,
+                                            color: Colors.black
+                                          ),
+                                        )
+                                      )
+                                    ]
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
                         )
-                      )
-                    ],
-                  ),
+                      ],
+                    ),
+                  )
                 )
               ),
-              Container(
-                height: 120+MediaQuery.of(context).padding.bottom,
-                decoration: BoxDecoration(
-                  color: Colors.grey[100],
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10)
-                  )
-                ),
-                child: Row(
-                  children: [
-                    
-                  ],
+              Obx(()=>
+                Container(
+                  height: 120+MediaQuery.of(context).padding.bottom,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[100],
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10)
+                    )
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: (){
+                          p.handler.skipToPrevious();
+                        },
+                        child: const Icon(
+                          Icons.skip_previous_rounded,
+                          size:30,
+                        ),
+                      ),
+                      const SizedBox(width: 20,),
+                      GestureDetector(
+                        onTap: (){
+                          if(p.nowPlay["id"].isNotEmpty){
+                            if(p.isPlay.value){
+                              p.handler.pause();
+                            }else{
+                              p.handler.play();
+                            }
+                          }
+                        },
+                        child: AnimatedContainer(
+                          height: 60,
+                          width: 60,
+                          duration: const Duration(milliseconds: 200),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: Colors.white,
+                            border: Border.all(
+                              color: Colors.black,
+                              width: 2
+                            )
+                          ),
+                          child: Icon(
+                            p.isPlay.value ? Icons.pause_rounded : Icons.play_arrow_rounded
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 20,),
+                      GestureDetector(
+                        onTap: (){
+                          p.handler.skipToNext();
+                        },
+                        child: const Icon(
+                          Icons.skip_next_rounded,
+                          size:30,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
               Container(
