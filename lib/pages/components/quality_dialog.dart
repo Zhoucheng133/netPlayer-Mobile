227d9a0a706af1241760d8d39fc4b0ce441bc06e
zhoucheng <zhoucheng133@outlook.com>
2025-04-24 20:53:15 +0800

    改进音质选择对话框

--- lib/pages/components/quality_dialog.dart
+++ lib/pages/components/quality_dialog.dart
@@ -1,23 +1,19 @@
 import 'dart:convert';
-
-import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
+import 'package:forui/forui.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:netplayer_mobile/variables/dialog_var.dart';
 import 'package:netplayer_mobile/variables/settings_var.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-void showQualityDialog(BuildContext context){
+Future<void> showQualityDialog(BuildContext context) async {
   final SettingsVar s=Get.find();
+  final DialogVar d=Get.find();
 
   final List<String> types=[
     '移动网络和无线网络',
     '仅移动网络',
-  ];
-
-  final List<String> qualities=[
-    '128Kbps',
-    '320Kbps'
   ];
 
   final List<String> qualitiesAll=[
@@ -28,116 +24,58 @@ void showQualityDialog(BuildContext context){
 
   CustomQuality local=s.quality.value;
 
-  showDialog(
+  final controller1 = FRadioSelectGroupController(value: local.cellularOnly ? '仅移动网络' : '移动网络和无线网络');
+  final controller2 = FRadioSelectGroupController(value: local.quality==0 ? '原始' : local.quality==320 ? '320Kbps' : '128Kbps');
+
+  await d.showOkCancelDialogRaw(
+    title: '修改音质',
     context: context,
-    builder: (context)=>AlertDialog(
-      title: const Text('修改音质'),
-      content: StatefulBuilder(
-        builder: (BuildContext context, StateSetter setState)=>Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            DropdownButtonHideUnderline(
-              child: Obx(()=>
-                DropdownButton2(
-                  isExpanded: true,
-                  value: local.cellularOnly ? '仅移动网络' : '移动网络和无线网络',
-                  items: types.map((String item) => DropdownMenuItem<String>(
-                    value: item,
-                    child: Text(
-                      item,
-                      style: GoogleFonts.notoSans(
-                        fontSize: 14,
-                      ),
-                    ),
-                  ))
-                  .toList(),
-                  onChanged: (val){
-                    if(val=='移动网络和无线网络'){
-                      setState(() {
-                        local.cellularOnly=false;
-                      });
-                    }else{
-                      if(local.quality==0){
-                        setState(() {
-                          local.quality=320;
-                        });
-                      }
-                      setState(() {
-                        local.cellularOnly=true;
-                      });
-                    }
-                  },
-                  dropdownStyleData: DropdownStyleData(
-                    decoration: BoxDecoration(
-                      color: s.darkMode.value ? s.bgColor1 : Colors.white,
-                    )
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 15,),
-            DropdownButtonHideUnderline(
-              child: Obx(()=>
-                DropdownButton2(
-                  isExpanded: true,
-                  value: local.quality==0 ? '原始' : local.quality==320 ? '320Kbps' : '128Kbps',
-                  items: local.cellularOnly ? qualities.map((String item) => DropdownMenuItem<String>(
-                    value: item,
-                    child: Text(
-                      item,
-                      style: const TextStyle(
-                        fontSize: 14,
-                      ),
-                    ),
-                  ))
-                  .toList() : qualitiesAll.map((String item) => DropdownMenuItem<String>(
-                    value: item,
-                    child: Text(
-                      item,
-                      style: const TextStyle(
-                        fontSize: 14,
-                      ),
-                    ),
-                  ))
-                  .toList(),
-                  onChanged: (val){
-                    if(val=='原始'){
-                      setState(() {
-                        local.quality=0;
-                      });
-                    }else if(val=='128Kbps'){
-                      setState(() {
-                        local.quality=128;
-                      });
-                    }else{
-                      setState(() {
-                        local.quality=320;
-                      });
-                    }
-                  },
-                  dropdownStyleData: DropdownStyleData(
-                    decoration: BoxDecoration(
-                      color: s.darkMode.value ? s.bgColor1 : Colors.white,
-                    )
-                  ),
-                ),
-              ),
-            )
-          ],
-        )
-      ),
-      actions: [
-        TextButton(
-          onPressed: () async {
-            s.quality.value=local;
-            s.quality.refresh();
-            Navigator.pop(context);
-            final SharedPreferences prefs = await SharedPreferences.getInstance();
-            await prefs.setString('quality', jsonEncode(s.quality.value.toJson()));
-          }, 
-          child: const Text('完成')
-        )
-      ],
-    )
+    child: StatefulBuilder(
+      builder: (BuildContext context, StateSetter setState)=>Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FSelectMenuTile(
+            groupController: controller1,
+            title: ListenableBuilder(
+              listenable: controller1,
+              builder: (_, context) {
+                return Text(controller1.value.first, style: GoogleFonts.notoSansSc(),);
+              }
+            ), 
+            menu: types.map((item)=>FSelectTile(
+              value: item,
+              title: Text(item, style: GoogleFonts.notoSansSc(),),
+            )).toList(),
+            autoHide: true,
+          ),
+          const SizedBox(height: 15,),
+          FSelectMenuTile(
+            groupController: controller2,
+            title: ListenableBuilder(
+              listenable: controller2,
+              builder: (_, context) {
+                return Text(controller2.value.first, style: GoogleFonts.notoSansSc(),);
+              }
+            ), 
+            menu: qualitiesAll.map((item)=>FSelectTile(
+              title: Text(item, style: GoogleFonts.notoSansSc(),), 
+              value: item
+            )).toList(),
+            autoHide: true,
+          ),
+        ],
+      )
+    ),
+    okHandler: () async {
+      local.cellularOnly=controller1.value.first=='仅移动网络';
+      local.quality=controller2.value.first=='原始' ? 0 : controller2.value.first=='320Kbps' ? 320 : 128;
+      s.quality.value=local;
+      s.quality.refresh();
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString('quality', jsonEncode(s.quality.value.toJson()));
+    }
   );
+
+  controller1.dispose();
+  controller2.dispose();
 }
